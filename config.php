457dc35b2b7e87e0d3a85b39129c1b8dffe53270
Quailty Ventures <47635254+qualityventures@users.noreply.github.com>
2019-06-14 15:08:48 -0400

    Update and rename config.example.php to config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+
+<?php
+// see README.md for an explanation of these config params
+$TWILIO_ACCOUNT_SID = 'AC6dd0eeab9d76a842b9ef982563bae6d7';
+$TWILIO_API_KEY = 'SK03e5bc8ade9fe113d174fadbf2fd8437';
+$TWILIO_API_SECRET = 'P29ExbLAYMW1wY0mzwTCtw0ry4obbkg1';
