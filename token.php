bada7a3dc4900533522da9c92498719922992e19
Quailty Ventures <47635254+qualityventures@users.noreply.github.com>
2019-06-14 16:11:10 -0400

    Update token.php

--- token.php
+++ token.php
@@ -1,6 +1,5 @@
 <?php
 include('./vendor/autoload.php');
-include('./config.php');
 
 use Twilio\Jwt\AccessToken;
 use Twilio\Jwt\Grants\VideoGrant;
@@ -8,6 +7,10 @@ use Twilio\Jwt\Grants\VideoGrant;
 // Use identity and room from query string if provided
 $identity = isset($_GET["identity"]) ? $_GET["identity"] : "identity";
 $room = isset($_GET["room"]) ? $_GET["room"] :  "";
+
+$TWILIO_ACCOUNT_SID = 'AC6dd0eeab9d76a842b9ef982563bae6d7';
+$TWILIO_API_KEY = 'SK03e5bc8ade9fe113d174fadbf2fd8437';
+$TWILIO_API_SECRET = 'P29ExbLAYMW1wY0mzwTCtw0ry4obbkg1';
 
 // Create access token, which we will serialize and send to the client
 $token = new AccessToken(
